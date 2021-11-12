a3344185e0051ea07f7128557c68123371744714
aykut <73406165+atmacaaykut@users.noreply.github.com>
2021-11-12 12:11:51 +0300

    v1

---
+++ PASSWORD GENERATOR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();                //[48-57]  [65-90]
+
+            string metin = "";
+
+            char karakter='a';
+
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                int hs = r.Next(1, 3);
+                if (hs==1)
+                {
+                    karakter = Convert.ToChar(r.Next(65, 91));
+                    metin += karakter;
+                }
+                else if (hs==2)
+                {
+                    karakter = Convert.ToChar(r.Next(48, 58));
+                    metin += karakter;
+                }
+                
+            }
+            Console.Write(metin);
+            
+        }
+
+       
+    }
+}
